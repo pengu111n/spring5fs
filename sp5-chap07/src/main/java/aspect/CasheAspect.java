ecd95173c0be8bfa021da5874460ec87178f4e79
pengu111n <100851664+pengu111n@users.noreply.github.com>
2022-07-24 03:39:39 +0900

    연습

---
+++ sp5-chap07/src/main/java/aspect/CasheAspect.java
@@ -0,0 +1,24 @@
+package aspect;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class CasheAspect {
+	
+	private Map<Long, Object> cache = new HashMap<>();
+	
+	@Pointcut("execution(public * chap07 ..*(long))")
+	public void cacheTarget() {}
+	
+	@Around("cacheTarget()")
+	public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
+		Long num = (Long)joinPoint.getArgs()[0];
+		if(cache.containsKey(num))
+	}
+}
--- sp5-chap07/src/main/java/main/MainAspect.java
+++ sp5-chap07/src/main/java/main/MainAspect.java
@@ -3,6 +3,7 @@ package main;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import chap07.Calculator;
+import chap07.RecCalculator;
 import config.AppCtx;
 
 public class MainAspect {
@@ -10,7 +11,8 @@ public class MainAspect {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
 		
-		Calculator cal = ctx.getBean("calculator", Calculator.class);
+//수정전 	Calculator cal = ctx.getBean("calculator", Calculator.class);
+		RecCalculator cal = ctx.getBean("calculator", RecCalculator.class); //수정후
 		long fiveFact = cal.factorial(5);
 		System.out.println("cal.factorial = " + fiveFact);
 		System.out.println(cal.getClass().getName());
