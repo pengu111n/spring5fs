08894ef2e8ec010a2a2ed0e05d7e32df4a74c375
pengu111n <xodhks8206@gmail.com>
2022-07-24 05:54:09 +0900

    연습

--- sp5-chap07/src/main/java/aspect/CacheAspect.java
+++ sp5-chap07/src/main/java/aspect/CacheAspect.java
@@ -9,7 +9,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
-public class CasheAspect {
+public class CacheAspect {
 	
 	private Map<Long, Object> cache = new HashMap<>();
 	
@@ -19,6 +19,13 @@ public class CasheAspect {
 	@Around("cacheTarget()")
 	public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
 		Long num = (Long)joinPoint.getArgs()[0];
-		if(cache.containsKey(num))
+		if(cache.containsKey(num)) {
+			System.out.printf("CacheAspect: Cache에서 구함 [%d]\n", num);
+			return cache.get(num);
+		}
+		Object result = joinPoint.proceed();
+		cache.put(num, result);
+		System.out.printf("CacheAspect: Cache에 추가[%d]\n", num);
+		return result;
 	}
 }
