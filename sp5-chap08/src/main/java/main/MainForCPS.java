08887e89641a4f4665b474153a1f7e6653fbb688
pengu111n <xodhks8206@gmail.com>
2022-07-26 06:27:57 +0900

    연습

--- sp5-chap08/src/main/java/main/MainForCPS.java
+++ sp5-chap08/src/main/java/main/MainForCPS.java
@@ -14,7 +14,7 @@ public class MainForCPS {
 		ChangePasswordService cps = ctx.getBean(ChangePasswordService.class);
 		
 		try {
-			cps.changePassword("a@a", "1234", "1111");
+			cps.changePassword("a@a", "1111", "1234");
 			System.out.println("암호를 변경했습니다.");
 		}catch(MemberNotFoundException e) {
 			System.out.println("회원데이터가 존재하지 않습니다.");
